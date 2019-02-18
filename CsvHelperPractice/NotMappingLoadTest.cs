a194b0d29846bcfc4a492af03e8613385c1910fe
RYOHEIMIURAD197\rim <mot_siva_ordin@hotmail.com>
2019-02-19 08:47:55 +0900

    CsvHelperのマッピングなしの読み込み処理を試した

---
+++ CsvHelperPractice/NotMappingLoadTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using NUnit.Framework;
+
+namespace CsvHelperPractice
+{
+    [TestFixture]
+    public class NotMappingLoadTest
+    {
+        /// <summary>
+        /// ルートディレクトリ
+        /// </summary>
+        public static string RootDirectory => Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+            , @"..\..");
+        /// <summary>
+        /// 計測用のインデックスのパス
+        /// </summary>
+        /// <returns></returns>
+        private static string CsvLibraryPath()
+        {
+            return Path.Combine(RootDirectory, "Csv", "KEN_ALL.CSV");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void LoadToArray()
+        {
+
+            var csvFilePath = CsvLibraryPath();
+            Assert.IsTrue(File.Exists(csvFilePath));
+            var encoding = Encoding.GetEncoding("Shift-JIS");
+            using (var reader = new CsvReader(new StreamReader(csvFilePath, encoding))
+            {
+                Configuration = { HasHeaderRecord = false}
+            })
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(string.Join(",", reader.Context.Record));
+                }
+            }
+        }
+       /// <summary>
+       /// 
+       /// </summary>
+       [Test]
+        public void LoadToDictionary()
+        {
+
+            var csvFilePath =Path.Combine(RootDirectory, "Csv", "mushrooms.csv");
+            Assert.IsTrue(File.Exists(csvFilePath));
+            var encoding = Encoding.GetEncoding("Shift-JIS");
+            using (var reader = new CsvReader(new StreamReader(csvFilePath, encoding))
+            {
+                Configuration = { HasHeaderRecord = false}
+            })
+            {
+                while (reader.Read())
+                {
+                    
+                var dictionary = reader.GetRecord<dynamic>() as IDictionary<string, object>;
+                var stringBuilder = new StringBuilder();
+                foreach (var pair in dictionary)
+                {
+                    stringBuilder.Append($"{pair.Key}->{pair.Value},");
+                }
+                Console.WriteLine(stringBuilder.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ CsvHelperPractice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsvHelperPractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsvHelperPractice")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c10b8451-7b42-479b-b1d1-51fa334f8d88")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
